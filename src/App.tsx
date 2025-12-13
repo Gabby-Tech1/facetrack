2d6f6ecabbedc09b801f91abdcbc57efc15a27d1
Dickson Daniel Peprah <techpepson@gmail.com>
2025-12-13 20:04:02 +0000

    feat(login screen): complete the login screen with design and styling

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import { Route, Routes } from "react-router-dom";
 import Login from "./pages/auth/login";
 import { ThemeProvider } from "next-themes";
 import "@radix-ui/themes/styles.css";
+import Dashboard from "./pages/dashboard/dashboard";
 
 const App: React.FC = () => {
   return (
     <ThemeProvider defaultTheme="dark" attribute="class">
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </ThemeProvider>
   );
